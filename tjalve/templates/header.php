ba226238d226bfcb0fabd44a3fc21983f05ebd57
Ingelhag <hannesingelhag@hotmail.com>
2014-02-17 09:14:05 +0100

    Styrt upp  dok i  mapppar version 1

--- tjalve/templates/header.php
+++ tjalve/templates/header.php
@@ -6,6 +6,6 @@
 		<title>
 			 Tjalve
 		</title>
-		<link rel="stylesheet" type="text/css" href="tjalvestyle.css" />
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
 		
 	</head>
