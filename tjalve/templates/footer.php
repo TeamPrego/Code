1e313d19b7267de9e406b07a90e7335b453e8501
byggprojektledarn <ctrl-z@live.se>
2014-02-17 14:59:10 +0100

    update footer

--- tjalve/templates/footer.php
+++ tjalve/templates/footer.php
@@ -1,13 +1,21 @@
 <!--Detta är en så kallad footar man-->
 	<div class="footer">
+	
+<script>
+	function login()
+	{
+	alert("Logga in då!");
+	}
+</script>
+	
 		<ul class="left-links">
 	        <li>© 2014 Prego Inc.</li>
-	        <li>070007007</li>		
+	        <li>070-1337133</li>		
 	        <li>Kontaktperson@kontakt.nu</li>	
 		</ul>
 		
 		<ul class="right-links">
-	        <li><a href="https://status.github.com/">Logga in</a></li>
+	        <li><a href onclick ="login()">Logga in</a></li>
 		</ul>
 	</div>
 	</body>
