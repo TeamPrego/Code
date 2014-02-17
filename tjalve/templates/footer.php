6c51624f650a4bedd869bb6b3e6321ebdc269aca
HerrLingvistik <gusha495@studen.liu.se>
2014-02-17 11:13:05 +0100

    la till footer och ändrade css
    footer finns och cssen är anpassad till denna

---
+++ tjalve/templates/footer.php
@@ -0,0 +1,7 @@
+<!--Detta är en så kallad footar man-->
+	<div class="footer">
+		<ul class="left-links">tjalve.@rastamail.com</ul>
+		<ul class="right-links">All rights reserved $$$$$$</ul>
+	</div>
+	</body>
+</html>
