2c8759ac8f730375e1f086d80b8313dfbe49e4c7
byggprojektledarn <ctrl-z@live.se>
2014-02-17 11:56:01 +0100

    Ändrade lite i footer och CSS

--- tjalve/templates/footer.php
+++ tjalve/templates/footer.php
@@ -1,7 +1,16 @@
 <!--Detta är en så kallad footar man-->
 	<div class="footer">
-		<ul class="left-links">tjalve.@rastamail.com</ul>
-		<ul class="right-links">All rights reserved $$$$$$</ul>
+		<ul class="left-links">
+	        <li>© 2014 Prego Inc.</li>
+	        <li><a href="https://status.github.com/">Money</a></li>
+	        <li><a href="https://status.github.com/">Heroes</a></li>			
+		</ul>
+		
+		<ul class="right-links">
+			<li><a href="https://status.github.com/">Hustle</a></li>
+	        <li><a href="https://status.github.com/">Work</a></li>
+	        <li><a href="https://status.github.com/">Kill</a></li>
+		</ul>
 	</div>
 	</body>
 </html>
