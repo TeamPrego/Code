a3c050d60576d1471d2f776a12d81bdbd3e05a18
Ingelhag <hannesingelhag@hotmail.com>
2014-02-17 21:14:21 +0100

    Version 1.02
    Första utkastet - med typ allt

--- tjalve/templates/footer.php
+++ tjalve/templates/footer.php
@@ -1,23 +1,22 @@
 <!--Detta är en så kallad footar man-->
-			<div class="footer">
+		</div>
+		<div class="footer">		
+			<script>
+				function login()
+				{
+				alert("Logga in då!");
+				}
+			</script>
+		
+			<ul class="left-links">
+		        <li>© 2014 Prego Inc.</li>
+		        <li>070-1337133</li>		
+		        <li>Kontaktperson@kontakt.nu</li>	
+			</ul>
 			
-		<script>
-			function login()
-			{
-			alert("Logga in då!");
-			}
-		</script>
-			
-				<ul class="left-links">
-			        <li>© 2014 Prego Inc.</li>
-			        <li>070-1337133</li>		
-			        <li>Kontaktperson@kontakt.nu</li>	
-				</ul>
-				
-				<ul class="right-links">
-			        <li><a href onclick ="login()">Logga in</a></li>
-				</ul>
-			</div>
+			<ul class="right-links">
+		        <li><a href onclick ="login()">Logga in</a></li>
+			</ul>
 		</div>
 	</body>
 </html>
