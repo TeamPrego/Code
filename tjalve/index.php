263712034928c13c50f04d04045526e1f5b81166
Emma <emmed608@student.liu.se>
2014-02-24 14:47:52 +0100

    Ändringar så koden följer Google Style Guide.

--- tjalve/index.php
+++ tjalve/index.php
@@ -1,35 +1,36 @@
 
 <?php
-include "templates/header.php";
+	include "templates/header.php";
 ?>	
-		<h1>Tjalve Friidrott</h1>
-		
-		<hr>
-		<table class ="firstTableList">
-			<thead>
-				<tr>
-					<th>Tävlingsnamn</th>
-					<th>Anmälan</th>
-					<th>Startlista</th>
-					<th>Reslutatlista</th>
-			</thead>	
-			<tbody>
-				<tr class="even">
-					<td> Tjalvesupertävling </td>
-					<td> Yesbox </td>
-					<td> Yesbox </td>
-					<td> Yesbox </td>
-				</tr>
+<h1>Tjalve Friidrott</h1>
 
-				<tr class="odd">
-					<td> Hannes-super-tävling </td>
-					<td> Yesbox </td>
-					<td> Ligger ej uppe </td>
-					<td> Ligger ej uppe </td>
-				</tr>
-			</tbody>
-		</table>
+<hr>
+<table class ="firstTableList">
+	<thead>
+		<tr>
+			<th>Tävlingsnamn</th>
+			<th>Anmälan</th>
+			<th>Startlista</th>
+			<th>Reslutatlista</th>
+	</thead>	
+	
+	<tbody>
+		<tr class="even">
+			<td> Tjalvesupertävling </td>
+			<td> Yesbox </td>
+			<td> Yesbox </td>
+			<td> Yesbox </td>
+		</tr>
+
+		<tr class="odd">
+			<td>Hannes-super-tävling</td>
+			<td>Yesbox</td>
+			<td>Ligger ej uppe</td>
+			<td>Ligger ej uppe</td>
+		</tr>
+	</tbody>
+</table>
 
 <?php
-include "templates/footer.php";
+	include "templates/footer.php";
 ?>
