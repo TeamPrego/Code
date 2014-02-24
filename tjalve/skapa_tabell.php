3b6f7dede456de2a62f2955e32ecd64235d53469
HerrLingvistik <gusha495@studen.liu.se>
2014-02-24 11:39:38 +0100

    Ändrade adresser
    Uppkoppling till Hannes databas

--- tjalve/skapa_tabell.php
+++ tjalve/skapa_tabell.php
@@ -2,32 +2,27 @@
 		include("resultat.php");
 		session_start();	
 	
-		$con=mysqli_connect("localhost","root","","tjalve");
+		$con=mysqli_connect("https://competition-192031.phpmyadmin.mysql.binero.se/","192031_jv80473","TeamPrego","192031-competition");
+		/*mysql_query("SET NAMES utf8");
+		  mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
+		  mysql_query("SET CHARACTER_SET utf8");*/	
 		// Check connection
 		if (mysqli_connect_errno())
 		  {
 		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		  }
+		  /*mysql_query("SET NAMES utf8");
+		  mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
+		  mysql_query("SET CHARACTER_SET utf8");*/
+/*mysql_query("SET CHARACTER SET 'utf8'") or die(mysql_error()); */
+		//mysql_query("SET caracter_set_results='utf8'") or die(mysql_error());
 		  if (isset($_POST["drop"])) 
 			{
-				$choice = mysql_real_escape_string($_POST["drop"]); //$_POST["drop"];
+			if($_POST["drop"] != "noChoice"){
+				$choice = mysql_real_escape_string($_POST["drop"]);
 				echo $choice;
 				echo " is choice";
-			} 
-			else 
-			{
-					$choice = null;
-				echo "no choice supplied";
-			}
-			//var_dump($_POST);
-		//$selectedText = $("#dropdown option:selected").text();
-		/*if($_POST['submit']){
-		
-			$gren = $_POST['formGender'];
-			$result = mysqli_query($con,"SELECT * FROM $_GET[]");
-			
-		}*/
-		$result = mysqli_query($con,"SELECT * FROM $choice");
+				$result = mysqli_query($con,"SELECT * FROM $choice");
 		
 		$columns = array();
 		
@@ -64,5 +59,20 @@
 		
 
 		mysqli_close($con);
+			}} 
+			else 
+			{
+				$choice = null;
+				echo "no choice supplied";
+			}
+			//var_dump($_POST);
+		//$selectedText = $("#dropdown option:selected").text();
+		/*if($_POST['submit']){
+		
+			$gren = $_POST['formGender'];
+			$result = mysqli_query($con,"SELECT * FROM $_GET[]");
+			
+		}*/
+		
 		
 	?>
