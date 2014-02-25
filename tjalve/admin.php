33caba8af116c6843e85025579e63227f2d893c4
byggprojektledarn <ctrl-z@live.se>
2014-02-25 10:07:23 +0100

    Fixat med adminsida m.m

---
+++ tjalve/admin.php
@@ -0,0 +1,39 @@
+<!--This is a so called not swole admin man-->
+<?php
+include "templates/adminheader.php";
+?>
+<h4>Du är inloggad som administratör</h4>
+
+<table class ="adminTable">
+	<thead>
+		<tr>
+			<th>Tävling</th>
+			<th>Tävlande</th>
+			<th>Fakturering</th>
+		</tr>
+		
+		<tr>
+			<td><button class="button">Skapa</button></td>
+			<td><button class="button">Ändra</button></td>
+			<td><button class="button">Fakturera</button></td>
+		</tr>
+		
+		<tr>
+			<td><button class="button">Ändra</button></td>
+			<td><button class="button">Sen anmälan</button></td>
+		</tr>
+		
+		<tr>
+			<td><button class="button">Resultat</button></td>
+			<td><button class="button">Tävlingsnummer</button></td>
+		</tr>
+		
+		<tr>
+			<td><button class="button">Utskrift</button></td>
+		</tr>
+	</thead>
+</table>
+
+<?php
+include "templates/adminfooter.php";
+?>
---
+++ tjalve/templates/adminheader.php
@@ -0,0 +1,15 @@
+<!--This is a so called admin header man-->
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>
+			 Tjalve
+		</title>
+		<link rel="stylesheet" href="css/admin.css" />
+		
+	</head>
+	<body>
+		<div id = "innerBody">
+			<div id = "adminlogo"><img id ="adminpicid" src="pic/adminpic.jpg" /></div>
+			<hr>
