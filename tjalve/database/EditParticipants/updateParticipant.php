2529e71e57256da5dfd46c6e06813671b30b4a3b
Emma Edvardsson <emmed608@student.liu.se>
2014-03-19 08:48:00 +0100

    Redigera deltagare - admin

---
+++ tjalve/database/EditParticipants/updateParticipant.php
@@ -0,0 +1,17 @@
+<?php
+ob_start();
+	include "config.php";
+	echo "zup?";
+
+	$pId = $_GET['participantId'];
+	$updFName = $_POST['u_fName'];
+	$updLName = $_POST['u_lName'];
+	$updBYear = $_POST['u_bYear'];
+	
+	$queryUpdate = "UPDATE participant SET firstName = '$updFName', lastName = '$updLName', birthYear = '$updBYear' WHERE participantId = '$pId' ";
+	$data = mysqli_query($con, $queryUpdate);
+
+	header("Location: ../editParticipants.php");
+	mysqli_close($con);
+ob_flush();
+?>
