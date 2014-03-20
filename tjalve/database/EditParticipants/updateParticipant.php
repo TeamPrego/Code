926df5f4185a3b93597ec0f887763e5f40a93afb
Emma Edvardsson <emmed608@student.liu.se>
2014-03-20 11:02:54 +0100

    Nu går det att ändra namn och ålder på deltagarna

--- tjalve/database/EditParticipants/updateParticipant.php
+++ tjalve/database/EditParticipants/updateParticipant.php
@@ -1,17 +1,24 @@
 <?php
 ob_start();
-	include "database/config.php";
-	echo "zup?";
+	include "../config.php";
+	echo 'woo';
 
 	$pId = $_GET['participantId'];
 	$updFName = $_POST['u_fName'];
 	$updLName = $_POST['u_lName'];
 	$updBYear = $_POST['u_bYear'];
-	
-	$queryUpdate = "UPDATE participant SET firstName = '$updFName', lastName = '$updLName', birthYear = '$updBYear' WHERE participantId = '$pId' ";
-	$data = mysqli_query($con, $queryUpdate);
+	echo $updFName;
+	echo $pId;
+	echo $updLName;
+	echo $updBYear;
 
-	header("Location: editParticipants.php");
+	$queryUpdate = "UPDATE participant SET firstName = '$updFName', lastName = '$updLName', birthYear = '$updBYear' WHERE participantId = '$pId'";
+	$data = mysqli_query($con, $queryUpdate);
+	if (!mysqli_query($con,$queryUpdate)) {
+	  die('Error: ' . mysqli_error($con));
+	}
+
+	header("Location: ../../editParticipants.php");
 	mysqli_close($con);
 ob_flush();
 ?>
