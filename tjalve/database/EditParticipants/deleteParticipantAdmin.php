f6cb4bdb25aaa3346f93e27ed4821184b3064bd1
Emma Edvardsson <emmed608@student.liu.se>
2014-03-21 13:04:36 +0100

    edit participant osv

---
+++ tjalve/database/EditParticipants/deleteParticipantAdmin.php
@@ -0,0 +1,22 @@
+<?php
+ob_start();
+ include "../config.php";
+
+	if (!isset($_GET['participantId'])) {
+	    echo 'No ID was given...';
+	    exit;
+	}
+
+	$participantId = $_GET['participantId'];
+	$data = mysqli_query($con, "SELECT contactId FROM participant WHERE participantId='$participantId'");
+	$contactId = $data->fetch_object()->contactId;
+
+	$sql = "DELETE FROM participant WHERE participantId = $participantId";
+
+	if (!mysqli_query($con,$sql)) {
+	  die('Error: ' . mysqli_error($con));
+	}
+	mysqli_close($con);
+	header("Location: ../../editParticipants.php");
+ob_end_flush();
+?>
