d001b288cd09430f48a8ea1291d310fea100db57
byggprojektledarn <ctrl-z@live.se>
2014-03-21 11:34:58 +0100

    CSSfix

--- tjalve/database/getCompetition.php
+++ tjalve/database/getCompetition.php
@@ -13,11 +13,11 @@
 	$row = $data->fetch_object();
 	echo "<div id='competition'><h1>" . $row->compName . "</h1></div>";	
   // echo "<div id='compLogoDiv'>";
-  echo "<div id='bronk'>";
-	echo "<div id='competition'> Arrangör: " . $row->compArr . "</div>";	
-	echo "<div id='competition'> Tävlingsdatum: " . $row->compDate . "</div>";	
-	echo "<div id='competition'> Sista anmälningsdag: " . $row->compLastDate . "</div>";	
-	echo "</div>";
+  echo "<table id='bronk'>";
+	echo "<tr><td>Arrangör: </td><td>" . $row->compArr . "</td></tr>";	
+	echo "<tr><td>Tävlingsdatum: </td><td>" . $row->compDate . "</td></tr>";	
+	echo "<tr><td>Sista anmälningsdag: </td><td>" . $row->compLastDate . "</td></tr>";	
+	echo "</table>";
   // echo "</div>";
   echo "<img id ='compLogo' src=". $row->compIcon ." alt ='Image' />";
   
